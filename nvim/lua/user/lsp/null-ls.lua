1d9dc11bcb854f01012293ffa0544e219e2980f3
tehmj <michaelnavs@gmail.com>
2022-05-18 00:29:49 -0500

    :sparkles: fixed broken changes to neovim config

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -20,7 +20,7 @@ null_ls.setup({
 		diagnostics.eslint,
 	},
 	on_attach = function(client)
-		if client.resolved_capabilities.document_formatting then
+		if client.server_capabilities.document_formatting then
 			vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
 		end
 	end,
